0162134640e26b9a9641708b01cd2f7daab984de
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2024-03-12 11:01:39 +1300

    start of assessment

---
+++ 04_assessment/random question.py
@@ -0,0 +1,51 @@
+import random
+
+
+import math
+
+
+# checks that a number is an integer
+def int_check(question, low=None, high=None, exit_code=None):
+
+    # if any int is allowed
+    if low is None and high is None:
+        error = "Please enter an integer"
+
+    # if the number needs to be more than an
+    # integer (ie: rounds / 'high number')
+    elif low is not None and high is None:
+        error = (f"Please enter an integer that is"
+                 f"more than / equal to {low}")
+
+    # if the number needs to be between low and high
+    else:
+        error = (f"Please enter an integer that"
+                 f" is between {low} and {high} (inclusive)")
+
+    while True:
+        response = input(question).lower()
+
+        # check for inf mode / exit code
+        if response == exit_code:
+            return response
+
+        try:
+            response = int(response)
+
+            # check the int is not low
+            if low is not None and response < low:
+                print(error)
+
+            # check response is more than the low number
+            elif high is not None and response > high:
+                print(error)
+
+            # if response is valid, return it
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+# main routine

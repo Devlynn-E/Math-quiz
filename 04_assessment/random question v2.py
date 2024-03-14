5583368ed0dab9360ab2cedc2c77a75051eaf6fd
Devlynn-E <141595269+Devlynn-E@users.noreply.github.com>
2024-03-14 13:29:30 +1300

    added v2 of  random question gen
    v2 of question gen adds the option to change the max base and exponent for the questions.

--- 04_assessment/random question v2.py
+++ 04_assessment/random question v2.py
@@ -48,59 +48,38 @@ def int_check(question, low=None, high=None, exit_code=None):
             print(error)
 
 
-# generates a random logarithm question
 def question_gen(ans_input="x = ", max_base=10, max_expo=5):
     list = []
 
     expo = random.randint(0, max_expo)
     base = random.randint(2, max_base)
     argument = base ** expo
-
+    
     list.append(expo)
     list.append(base)
     list.append(argument)
 
     unknown = random.choice(list)
-
+    
     if unknown == argument:
         quiz = f"log{base}(x) = {expo}" \
                f"\n{ans_input}"
-
+        
         x = argument
-
+        
     elif unknown == base:
         quiz = f"logx({argument}) = {expo}" \
                f"\n{ans_input}"
-
+        
         x = base
-
+        
     else:
         quiz = f"log{base}({argument}) = x" \
                f"\n{ans_input}"
-
+        
         x = expo
 
     return quiz, x
-
-
-def instructions():
-    print('''
-
-        **** instructions ****
-
-        To begin, choose the number of rounds (or press <enter> for infinite mode).
-
-        You will then chose a lower and higher number (inclusive) that will contain your secret number 
-
-        You will then try to guess the number while the computer will give you hints for each guess
-
-        You will receive statistics on your guesses used and will be able to see your game history at the end of the game
-
-        Type <xxx> to end the game at anytime.
-
-        🎂🎂🎂Good Luck🎂🎂🎂
-
-            ''')
 
 
 def yes_no(question):
@@ -119,32 +98,6 @@ def yes_no(question):
 
 # main routine
 
-mode = "regular"
-rounds_played = 0
-end_game = "no"
-
-ans_his = []
-response_his = []
-
-print("\n📈📈📈 Math Quiz (Logs) 📉📉📉")
-print()
-
-# instructions
-wants_instructions = yes_no("Do you want to view the instructions? ")
-
-if wants_instructions == "yes":
-    # displays instructions
-    instructions()
-
-num_rounds = int_check("\nHow many questions? <enter for infinite>: ", low=1, exit_code="")
-
-if num_rounds == "":
-    mode = "infinite"
-    rounds_to_play = 37
-
-else:
-    rounds_to_play = num_rounds
-
 default_params = yes_no("\nDo you want to use the default log parameters? ")
 if default_params == "no":
     base_input = int_check("\nmax base? ", low=2)
@@ -157,11 +110,12 @@ else:
     base_input = 10
     expo_input = 5
 
-while True:
+quiz_1 = question_gen("x = ", base_input, expo_input)
+ans_1 = quiz_1[1]
+print(ans_1)
+ques_1 = int_check(quiz_1[0])
 
-    quiz_1 = question_gen("x = ", base_input, expo_input)
-    ans_1 = quiz_1[1]
-    ques_1 = int_check(quiz_1[0])
+while True:
 
     if ques_1 == ans_1:
         print("Correct")
